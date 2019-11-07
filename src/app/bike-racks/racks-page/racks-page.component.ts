43b91ec8c0766c342c100f2db9859a6866b3abfd
i.ivanov <i.ivanov@lucky-labs.com>
2019-11-07 18:48:26 +0200

    add user position customized marker

--- src/app/bike-racks/racks-page/racks-page.component.ts
+++ src/app/bike-racks/racks-page/racks-page.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {GoogleMap} from "@angular/google-maps";
 import {BikeRack} from "../bike-rack";
 import {data} from "../racks-data";
 
@@ -9,43 +10,58 @@ import {data} from "../racks-data";
 })
 export class RacksPageComponent implements OnInit {
 
-    mapCenter: google.maps.LatLngLiteral;
+    userPosition: Position;
     zoom: number = 15;
-    mapOptions: google.maps.MapOptions = {
-        minZoom: 11,
-        maxZoom: 21,
-        streetViewControl: false,
-        fullscreenControl: false,
-        panControl: false,
-        mapTypeControl: false
+
+    mapOptions: google.maps.MapOptions;
+
+    userMarkerOptions: google.maps.MarkerOptions = {
+        icon: '/assets/user-dot.svg',
+        visible: true
     };
 
     racks: BikeRack[];
 
-    private readonly initialKyivCoords: google.maps.LatLngLiteral = {
+    @ViewChild('googleMap', {read: GoogleMap}) mapRef: GoogleMap;
+
+    private readonly KyivCenterCoords: google.maps.LatLngLiteral = {
         lat: 50.449834,
         lng: 30.523799
     };
 
+    private readonly minZoom = 11;
+    private readonly maxZoom = 19;
+
     constructor() {
-        this.mapCenter = this.initialKyivCoords;
+        this.mapOptions = {
+            center: this.KyivCenterCoords,
+            minZoom: this.minZoom,
+            maxZoom: this.maxZoom,
+            streetViewControl: false,
+            fullscreenControl: false,
+            panControl: false,
+            mapTypeControl: false
+        };
         this.racks = data;
     }
 
     ngOnInit() {
-        this.centerMapToUserPosition();
+
     }
 
     centerMapToUserPosition(): void {
         if(navigator.geolocation) {
             const onSuccess = position => {
-                this.mapCenter = {
+                this.mapRef.panTo({
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
-                };
+                });
+                this.mapRef.zoom = this.maxZoom;
+                this.userPosition = position;
             };
             const onReject = () => {
-                this.mapCenter = this.initialKyivCoords;
+                this.mapRef.panTo(this.KyivCenterCoords);
+                this.userPosition = null;
             };
             navigator.geolocation.getCurrentPosition(onSuccess, onReject);
         }
